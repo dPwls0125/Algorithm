c35ce9c551b0d1927bc6666a1f17df78a0d7df24
kimYejin <kimphoby0125@gmail.com>
2024-07-24 16:21:22 +0900

    dfs : 타깃 넘버.cpp

---
+++ 프로그래머스/Level2/타겟 넘버.cpp
@@ -0,0 +1,38 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int result = 0;
+int global_target;
+
+void dfs(int idx, long size, int calculation, vector<int> numbers){
+    
+    if((idx > size-1) ){
+        if(calculation == global_target) ++result;
+        return;
+    }
+    
+    ++idx;
+
+    dfs(idx,size,calculation-numbers[idx-1],numbers);
+    dfs(idx,size,calculation+numbers[idx-1],numbers);
+    
+}
+
+int solution(vector<int>numbers, int target) {
+    global_target = target;
+    dfs(0,numbers.size(),0,numbers);
+    return result;
+}
+
+
+int main(void){
+   vector<int>numbers = {4, 1, 2, 1};
+   int target = 4;
+   cout << solution(numbers,target);
+
+
+
+}
