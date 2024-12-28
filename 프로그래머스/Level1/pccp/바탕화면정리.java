d377deb05637611ea4868a3040ed99f5906a9186
kimYejin <kimphoby0125@gmail.com>
2024-12-28 22:29:14 +0900

    refact:level1 : 바탕화면 정리

--- 프로그래머스/Level1/pccp/바탕화면정리.java
+++ 프로그래머스/Level1/pccp/바탕화면정리.java
@@ -2,66 +2,25 @@ package 프로그래머스.Level1.pccp;
 
 public class 바탕화면정리 {
     public int[] solution(String[] wallpaper) {
+
+        int top = Integer.MAX_VALUE;
+        int bottom = Integer.MIN_VALUE;
+        int left = Integer.MAX_VALUE;
+        int right = Integer.MIN_VALUE;
+
         int[] answer = {};
 
-        int top = findTop(wallpaper);
-        int bottom = findBottom(wallpaper);
-        int left = findLeft(wallpaper);
-        int right = findRight(wallpaper);
-
+        for(int i=0; i<wallpaper.length;i++){
+            for(int j=0; j<wallpaper[i].length(); j++){
+                if(wallpaper[i].charAt(j) == '#'){
+                    top = Math.min(top,i);
+                    bottom = Math.max(bottom,i+1);
+                    left = Math.min(left,j);
+                    right = Math.max(right,j+1);
+                }
+            }
+        }
         answer = new int[]{top, left, bottom, right};
         return answer;
-    }
-
-    public int findTop(String[] wallpaper){
-        int top = 50;
-        for(int i=0; i< wallpaper.length; i++){
-            for(int j=0; j<wallpaper[0].length(); j++){
-                char c = wallpaper[i].charAt(j);
-                if(c == '#' && i < top) {
-                    top = i;
-                }
-            }
-        }
-        return top;
-    }
-
-    public int findBottom(String[] wallpaper){
-        int bottom = 0;
-        for(int i=0; i< wallpaper.length; i++){
-            for(int j=0; j<wallpaper[0].length(); j++){
-                char c = wallpaper[i].charAt(j);
-                if(c == '#' && i > bottom) {
-                    bottom = i;
-                }
-            }
-        }
-        return bottom + 1 ;
-    }
-
-    public int findLeft(String[] wallpaper){
-        int left = 50;
-        for(int i=0; i<wallpaper.length; i++){
-            for(int j=0; j<wallpaper[0].length(); j++){
-                char c = wallpaper[i].charAt(j);
-                if(c == '#' && j < left) {
-                    left = j;
-                }
-            }
-        }
-        return left;
-    }
-
-    public int findRight(String[] wallpaper){
-        int right = 0;
-        for(int i=0; i<wallpaper.length; i++){
-            for(int j=0; j<wallpaper[0].length(); j++){
-                char c = wallpaper[i].charAt(j);
-                if(c == '#' && j > right) {
-                    right = j;
-                }
-            }
-        }
-        return right + 1 ;
     }
 }
